23b019da0392d53b5bc89849eb6733817a3976fe
Maksim_Mozhorov@epam.com <Maksim_Mozhorov@epam.com>
2020-07-06 21:11:57 +0300

    Finished transfer count filter

--- backend/src/services/tickets.service.ts
+++ backend/src/services/tickets.service.ts
@@ -1,21 +1,25 @@
+import { paginationFormatter, transferCountFormatter } from "../utils/queryFormatter";
+
 const ticketModel = require('../models/ticket.model');
 
 export class TicketsService{
     static async getTickets( queries ){
         const {
-            limit = 20,
-            offset = 0,
-            currency,
-            stops
+            currency
         } = queries;
+
+        const { limit, offset } = paginationFormatter(queries);
+        const stops = transferCountFormatter(queries);
 
         const queryParams = {};
 
         if (typeof currency !== 'undefined')
             Object.assign(queryParams, { currency });
-        if (typeof stops !== 'undefined')
-            Object.assign(queryParams, { stops });
 
-        return await ticketModel.find(queryParams, { '_id': 0, '__v': 0 }).skip(Number(offset)).limit(Number(limit));
+
+        if(stops.length)
+            Object.assign(queryParams, { 'stops':  { $in:stops } });
+
+        return await ticketModel.find(queryParams, { '_id': 0, '__v': 0 }).skip(offset).limit(limit);
     }
 }
---
+++ backend/src/utils/queryFormatter.ts
@@ -0,0 +1,36 @@
+import {ALL} from "dns";
+
+const ALL_STOPS_FILTER = 'all';
+
+export const paginationFormatter = ( queries ) => {
+    const {
+        limit = 20,
+        offset = 0,
+    } = queries;
+
+    return{
+        limit: Number(limit),
+        offset: Number(offset)
+    };
+}
+
+export const transferCountFormatter = ( queries ) => {
+
+    const {
+        transferCount: stops
+    } = queries;
+
+    if (stops === undefined)
+        return [];
+
+    if (typeof stops === 'string')
+        if (stops === ALL_STOPS_FILTER)
+            return [];
+        else
+            return [stops];
+
+    if (stops .find( stop => ALL_STOPS_FILTER === stop) === undefined )
+        return stops;
+
+    return [];
+}
