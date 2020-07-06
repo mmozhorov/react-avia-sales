4e2ea98cc8206f43bdde6f62cd246e30d2a02882
Maksim_Mozhorov@epam.com <Maksim_Mozhorov@epam.com>
2020-07-06 19:31:57 +0300

    Finished stylized

--- frontend/src/components/Filter/TransferCountComponent.tsx
+++ frontend/src/components/Filter/TransferCountComponent.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 
-import { useFilters } from "../../hooks/useFilters";
 import { changeTransferCount } from "../../actions/filterActions";
 
-const CountOfTransfer = [ "Все", "Без пересадок", "1 пересадка", "2 пересадки", "3 пересадки" ];
+import './TransferCountComponent.css';
+
+const CountOfTransfer = [
+    { label: "Все", value: "all" },
+    { label: "Без пересадок", value: "none" },
+    { label: "1 пересадка", value: "1" },
+    { label: "2 пересадки", value: "2" },
+    { label: "3 пересадки", value: "3" },
+];
 
 export const TransferCountComponent = () => {
-    const { dispatch } = useFilters();
     return (
         <div>
             <h5>Количество пересадок</h5>
-            {CountOfTransfer.map( (item, i) => {
-                return <p  key={i}><input type="checkbox" value={item} onChange={ () => dispatch(changeTransferCount(item)) } />{item}</p>
-            })}
+            <div className="transfer-count-container">
+                {CountOfTransfer.map( (item, i) => {
+                    return(
+                        <React.Fragment>
+                            <input type="checkbox" value={item.value} id={`transfer-${i+1}`} />
+                            <label htmlFor={`transfer-${i+1}`}>{item.label}</label>
+                        </React.Fragment>
+                    )
+                    // return <p  key={i}><input type="checkbox" value={item} onChange={ () => dispatch(changeTransferCount(item)) } />{item}</p>
+                })}
+            </div>
         </div>
     );
 }
